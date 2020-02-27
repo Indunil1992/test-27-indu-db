0ad6ef99d2c75625391395ed3d9eeb903b4825db
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 13:02:37 +0530

    scan 1

--- lambda.js
+++ lambda.js
@@ -3,22 +3,18 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
     try {
-        let data = await ddb.query({
-            TableName: "hirutest",
-            ExpressionAttributeValues: {
-                ':@1': "NaN"
-            },
-            KeyConditionExpression: "price < :@1",
-            FilterExpression: "x = :@1"
+        let data = await ddb.scan({
+            TableName: "hirutest"
         }).promise();
         console.log("data");
-        console.log(data);
+                console.log(data);
     } catch (err) {
         // error handling goes here
-        console.log("err");
-        console.log(err);
+                console.log("err");
+                console.log(err);
 
     };
+
 
     return { "message": "Successfully executed timestamp" };
 };
