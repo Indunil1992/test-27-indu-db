2f033444aa5ccb2c5963566faab1410c740ba9b3
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 12:01:55 +0530

    Q 1

--- lambda.js
+++ lambda.js
@@ -3,17 +3,14 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
     try {
-        let data = await ddb.delete({
-            TableName: "indunil-DB-27",
-            Key: {
-                food: "1"
-            }
+        let data = await ddb.query({
+            TableName: "indunil-DB-27"
         }).promise();
-        console.log("data");
+ console.log("data");
                 console.log(data);
     } catch (err) {
         // error handling goes here
-        console.log("err");
+                console.log("err");
                 console.log(err);
 
     };
