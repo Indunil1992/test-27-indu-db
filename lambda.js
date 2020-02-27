311a49b299cb493067177ebd3147ad5fcb5bab9a
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 11:53:08 +0530

    delete

--- lambda.js
+++ lambda.js
@@ -1,5 +1,22 @@
+let AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+
 exports.handler = async (event) => {
-//cmnt
+    try {
+        let data = await ddb.delete({
+            TableName: "indunil-DB-27",
+            Key: {
+                food: "1"
+            }
+        }).promise();
+        console.log("data");
+                console.log(data);
+    } catch (err) {
+        // error handling goes here
+        console.log("err");
+                console.log(err);
+
+    };
 
     return { "message": "Successfully executed timestamp" };
 };
