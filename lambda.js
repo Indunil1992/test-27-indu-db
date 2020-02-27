ba745c65dc36aa8e0d2465fcd33f110c7c241db3
Indunil1992 <indunil@adroitlogic.com>
2020-02-27 11:28:32 +0530

    Trigger 1

--- lambda.js
+++ lambda.js
@@ -1,19 +1,5 @@
-let AWS = require('aws-sdk');
-const ddb = new AWS.DynamoDB.DocumentClient();
-
 exports.handler = async (event) => {
-    try {
-        let data = await ddb.get({
-            TableName: "indunil-DB-27",
-            Key: {
-                food: "cakes"
-            }
-        }).promise();
-        console.log(data);
-    } catch (err) {
-        // error handling goes here
-        console.log(err);
-    };
+
 
     return { "message": "Successfully executed" };
 };
